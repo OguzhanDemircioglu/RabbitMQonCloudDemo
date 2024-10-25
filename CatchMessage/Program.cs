b5ad211ad076476a11ba83b50a086458188a6b88
OguzhanAcer <oguzhanturgut611@gmail.com>
2024-10-25 16:37:15 +0300

    sender, receiver

--- CatchMessage/Program.cs
+++ CatchMessage/Program.cs
@@ -1,3 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
 
-Console.WriteLine("Hello, World!");
+namespace CatchMessage;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var factory = new ConnectionFactory
+        {
+            Uri = new Uri("amqps://uydkmrcy:eVwCFhNxrZ6BQDSgIJuMEr6ZGEpH6mwB@kangaroo.rmq.cloudamqp.com/uydkmrcy")
+        };
+
+        await using var connection = await factory.CreateConnectionAsync();
+        using var channel = await connection.CreateChannelAsync();
+
+        await channel.QueueDeclareAsync(queue: "test-queue", durable: true, exclusive: false, autoDelete: false);
+
+        var consumer = new AsyncEventingBasicConsumer(channel);
+
+        consumer.ReceivedAsync += async (sender, @event) =>
+        {
+            var message = Encoding.UTF8.GetString(@event.Body.ToArray());
+            Console.WriteLine("Gelen Mesaj: " + message);
+            await Task.Yield(); 
+        };
+
+        await channel.BasicConsumeAsync(queue: "test-queue", autoAck: true, consumer: consumer);
+
+        Console.WriteLine("Mesajlar dinleniyor...");
+        Console.ReadLine();
+    }
+}
--- RabbitSender/Program.cs
+++ RabbitSender/Program.cs
@@ -1,2 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text;
+using RabbitMQ.Client;
+
+namespace RabbitSender;
+
+internal class Program
+{
+    static async Task Main(string[] args)
+    {
+        var factory = new ConnectionFactory
+        {
+            Uri = new Uri("amqps://uydkmrcy:eVwCFhNxrZ6BQDSgIJuMEr6ZGEpH6mwB@kangaroo.rmq.cloudamqp.com/uydkmrcy")
+        };
+
+        await using var connection = await factory.CreateConnectionAsync();
+        using var channel = await connection.CreateChannelAsync();
+
+        await channel.QueueDeclareAsync(queue: "test-queue", durable: true, exclusive: false, autoDelete: false);
+
+        Console.WriteLine("RabbitMQ kuyruğuna mesaj göndermek için metin girin ('exit' yazarak çıkabilirsiniz):");
+
+        while (true)
+        {
+            var mesaj = Console.ReadLine();
+
+            if (mesaj?.ToLower() == "exit")
+                break;
+
+            var body = Encoding.UTF8.GetBytes(mesaj!);
+
+            await channel.BasicPublishAsync(
+                exchange: string.Empty,
+                routingKey: "test-queue",
+                mandatory: false,
+                body: new ReadOnlyMemory<byte>(body),
+                cancellationToken: CancellationToken.None
+            );
+
+            Console.WriteLine("Mesaj gönderildi: " + mesaj);
+        }
+
+        Console.WriteLine("Program sonlandırıldı.");
+    }
+}
